7e4f8765c1a1a2917000ea662b0acb6ecb93ed0f
Kody Samaroo <kody.samaroo@gmail.com>
2023-04-23 02:04:21 -0400

    product routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const productRouter = require("./routes/product");
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.json());
+app.use("/api/producst", productRouter);
 
 
 app.listen(process.env.PORT || 4000, () => {
--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,79 @@
 const router = require("express").Router();
+const Product = require("../models/Product");
+
+// CREATE
+router.post("/", async (req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET ALL PRODUCTS
+router.get("/", async (req, res) => {
+    const queryNew = req.query.new;
+    const queryCategory = req.query.category;
+    try {
+        let products;
+
+        if (queryNew) {
+            products = await Product.find().sort({ createdAt: -1 }).limit(1);
+        } else if (queryCategory) {
+            products = await Product.find({
+                categories: {
+                    $in: [queryCategory],
+                },
+            });
+        } else {
+            const products = await Product.find();
+        }
+
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router
